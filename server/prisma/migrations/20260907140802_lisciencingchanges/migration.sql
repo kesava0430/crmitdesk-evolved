c4467501d3990fa319731ec650b0533935276b49
kesava <kesava.harinath30@gmail.com>
2026-09-07 19:38:55 +0530

    License changes

---
+++ server/prisma/migrations/20260907140802_lisciencingchanges/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "deals_org_id_department_id_idx";
+
+-- DropIndex
+DROP INDEX "leads_org_id_department_id_idx";
+
+-- DropIndex
+DROP INDEX "tickets_org_id_department_id_idx";
